afd19572974acf85847375c2f2dca546e4b52f3d
Joshua Sharp <joshua@emptybliss.biz>
2023-10-15 13:26:33 -0400

    Initial Commit

---
+++ Todo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Todo
+//
+//  Created by Joshua Sharp on 10/15/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
